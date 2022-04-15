98d86a3e22bc865cb33d4e6d031194d987940a70
jsshin <jsshin6246@tsinfotech.co.kr>
2022-04-15 09:02:12 +0900

    style: 코드 컨베션 적용안된 클래스 적용

--- src/main/java/baseball/domain/BallGenerator.java
+++ src/main/java/baseball/domain/BallGenerator.java
@@ -11,36 +11,36 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class BallGenerator {
-	private final List<Integer> ballNumbers;
+    private final List<Integer> ballNumbers;
 
-	public BallGenerator() {
-		this.ballNumbers = new ArrayList<>();
-		createNonDuplicateNumbers();
-	}
+    public BallGenerator() {
+        this.ballNumbers = new ArrayList<>();
+        createNonDuplicateNumbers();
+    }
 
-	public List<Integer> getBallNumbers() {
-		return this.ballNumbers;
-	}
+    public List<Integer> getBallNumbers() {
+        return this.ballNumbers;
+    }
 
-	private void createNonDuplicateNumbers() {
-		while (!isListSizeEqualLimit()) {
-			int randomNumber = Randoms.pickNumberInRange(START_NUMBER.getNumber(), END_NUMBER.getNumber());
-			addNumber(randomNumber);
-		}
-	}
+    private void createNonDuplicateNumbers() {
+        while (!isListSizeEqualLimit()) {
+            int randomNumber = Randoms.pickNumberInRange(START_NUMBER.getNumber(), END_NUMBER.getNumber());
+            addNumber(randomNumber);
+        }
+    }
 
-	private boolean isListSizeEqualLimit() {
-		return this.ballNumbers.size() == SIZE_LIMIT_NUMBER.getNumber();
-	}
+    private boolean isListSizeEqualLimit() {
+        return this.ballNumbers.size() == SIZE_LIMIT_NUMBER.getNumber();
+    }
 
-	private void addNumber(int number) {
-		if (!isDuplicationNumber(number)) {
-			this.ballNumbers.add(number);
-		}
-	}
+    private void addNumber(int number) {
+        if (!isDuplicationNumber(number)) {
+            this.ballNumbers.add(number);
+        }
+    }
 
-	private boolean isDuplicationNumber(int number) {
-		return this.ballNumbers.contains(number);
-	}
+    private boolean isDuplicationNumber(int number) {
+        return this.ballNumbers.contains(number);
+    }
 
 }
